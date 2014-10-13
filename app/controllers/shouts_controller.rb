9218f093a9296d5368c5b06bbc79d90a22b4980e
Elizabeth Collins <escollins27@gmail.com>
2014-10-13 11:22:47 -0400

    Users can shout

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,13 +1,19 @@
 class ShoutsController < ApplicationController
   before_action :require_login
+
   def index
     @shout = Shout.new
     @shouts = Shout.all
   end
 
   def create
-    Shout.create(user_id: current_user.id, body:params[:shout][:body])
-
+   # Shout.create(user_id: current_user.id, body:params[:shout][:body])
+    current_user.shout.create(shout.params)
     redirect_to shouts_path
+  end
+
+  private
+  def shout_params
+    params.require(:shout).permit(:body)
   end
 end
