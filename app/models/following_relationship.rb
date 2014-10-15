f110900bfde2a3bf6730760a62747c7e1d71f82b
Elizabeth Collins <escollins27@gmail.com>
2014-10-15 15:51:05 -0400

    Shouts are paginated

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,14 +1,14 @@
 class FollowingRelationship < ActiveRecord::Base
   belongs_to :followed_user, class_name: "User"
+  belongs_to :follower, class_name: "User"
 
-
-validate:user_is_not_following_themself
+  validate:user_is_not_following_themself
 
   private
 
   def user_is_not_following_themself
-  if follower_id ==follower_user_id
-    errors.add(:base, "Users cannot follow themselves")
-  end
+    if follower_id == follower_user_id
+      errors.add(:base, "Users cannot follow themselves")
+    end
   end
 end
